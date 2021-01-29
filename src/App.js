ee5e4cd93e6fac3166dc250ee2466e401eef323a
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-29 02:05:01 -0300

    style: add ResetCSS and GlobalCSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import ResetCSS from './resetCSS'
+import GlobalStyle from './globalStyles'
 
 function App () {
-  return <h1>Home</h1>
+  return (
+    <>
+      <ResetCSS />
+      <GlobalStyle />
+
+      <h1>Home</h1>
+    </>
+  )
 }
 
 export default App
---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        background-color: #292929;
+        font-size: 16px;
+    }
+`
+
+export default GlobalStyles
