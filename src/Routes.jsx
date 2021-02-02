b1b96d264c6091b2918b2051f6af091fececfc71
Dijango Alves <dijango_ilha@hotmail.com>
2021-02-01 21:19:41 -0300

    feat: create hook to check private routes and add to routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import AuthProvider from './Context/AuthProvider'
+import PrivateRoutes from './Components/PrivateRoutes'
+
 import Login from './Pages/Login'
 import User from './Pages/User'
 import Repositories from './Pages/Repositories'
@@ -9,13 +12,15 @@ import Followers from './Pages/Followers'
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Login} />
-        <Route path='/user' component={User} />
-        <Route exact path='/repos' component={Repositories} />
-        <Route path='/followers' component={Followers} />
-        <Route path='*' component={Login} />
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <PrivateRoutes path='/user' component={User} />
+          <PrivateRoutes exact path='/repos' component={Repositories} />
+          <PrivateRoutes path='/followers' component={Followers} />
+          <Route path='*' component={Login} />
+        </Switch>
+      </AuthProvider>
     </Router>
   )
 }
