3379003c52da66fea0b772aa4a11a2fd54b3f0ca
Dijango Alves <dijango_ilha@hotmail.com>
2021-02-03 14:29:15 -0300

    chore: add new font family from Google Fonts

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
     body {
         background-color: #292929;
         font-size: 16px;
+        font-family: 'Roboto', sans-serif;
     }
 `
 
