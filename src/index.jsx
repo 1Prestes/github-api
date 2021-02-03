dd210d53aabef459132c67cf3a3b3a012404034b
Dijango Alves <dijango_ilha@hotmail.com>
2021-02-03 07:16:07 -0300

    fix: solve problem with find file

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-import ResetCSS from './ResetCSS'
-import GlobalStyle from './GlobalStyles'
+import ResetCSS from './ResetCSS.js'
+import GlobalStyle from './GlobalStyles.js'
 
 ReactDOM.render(
   <React.StrictMode>
